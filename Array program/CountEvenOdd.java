43aae887962c12cb78ae6a2ce5ae80c9921bb476
Rashmi rani <mrashmi791@gmail.com>
2018-12-10 21:46:36 +0530

    Modify program

--- Array program/CountEvenOdd.java
+++ Array program/CountEvenOdd.java
@@ -5,32 +5,24 @@ class CountEvenOdd {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter array size");
         int m = sc.nextInt();
-        int n = sc.nextInt();
-
-        int arr[][] = new int[m][n];
+        int arr[] = new int[m];
          
-
         System.out.println("enter array element");
         for (int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-               arr[i][j] = sc.nextInt();
-            }
+               arr[i] = sc.nextInt();
         }
          
          int even = 0;
          int odd = 0;
-         //System.out.println("Sum of the matrices :");
+         
         for(int i = 0; i< m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(arr[i][j] % 2 == 0){
+                if(arr[i] % 2 == 0){
                     even++;
                 }
                 else {
                     odd++;
                 }
                 
-            }
-            
         }
          System.out.println("Total even number is :" + even);
           System.out.println("Total odd number is : " +odd);
