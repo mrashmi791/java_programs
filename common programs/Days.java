1b7f45c37631ddc06a16d61c23210dcaaee67797
Rashmi rani <mrashmi791@gmail.com>
2018-12-12 19:29:51 +0530

    Displays the name of the weekday

---
+++ common programs/Days.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Days {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter choice");
+        int ch = sc.nextInt();
+
+        switch (ch) {
+        case 0:
+            System.out.println("Sunday");
+            break;
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        default:
+            System.out.println("Wrong choice");
+            break;
+        }
+
+    }
+}
