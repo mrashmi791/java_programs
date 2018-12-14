453cc69e12c3bba9a765cac7a84a3f929adab311
Rashmi rani <mrashmi791@gmail.com>
2018-12-14 13:30:24 +0530

    Encapsulation's program

---
+++ Practices/StudentDetails.java
@@ -0,0 +1,7 @@
+class StudentDetails {
+    public String name = "rashmi";
+    public int roll = 22;
+    String address = "patna";
+    private int marks = 520;
+    
+}
---
+++ Practices/school.java
@@ -0,0 +1,10 @@
+class school {
+    public static void main(String[] ar) {
+        System.out.println("This is information");
+        StudentDetails sd = new StudentDetails();
+        System.out.println(sd.name);
+        System.out.println(sd.roll);
+        System.out.println(sd.address);
+       // System.out.println(sd.marks);
+    }
+}
