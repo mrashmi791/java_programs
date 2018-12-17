d87dd177a7b235ab73dbd81d89f9c677abc6e96b
Rashmi rani <mrashmi791@gmail.com>
2018-12-17 13:24:52 +0530

    To check how the constructor is working

---
+++ Practices/check.java
@@ -0,0 +1,11 @@
+
+class Check {
+    Check() {
+        System.out.println("construster call");
+    }
+
+    public static void main(String[] ar) {
+        Check c = new Check();
+        System.out.println("after call cons");
+    }
+}
