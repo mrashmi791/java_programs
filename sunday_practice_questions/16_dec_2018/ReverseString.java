cdd55aceef777bb7ce584803a143d76e374c0432
Rashmi rani <mrashmi791@gmail.com>
2018-12-17 13:28:33 +0530

    To find first non-repeating element in array of integers

---
+++ sunday_practice_questions/16_dec_2018/ReverseString.java
@@ -0,0 +1,30 @@
+// to reverse string without using API(Predefined methods).
+
+
+class ReverseString {
+    public static void main(String[] ar) {
+        String name = "Rashmi";
+       
+        char str[] = name.toCharArray();
+        int length = str.length;
+
+        int left =0; 
+        int right = length - 1;
+
+        while(left < right){
+            char tmp  = str[left];
+            str[left] = str[right];
+            str[right] =  tmp;
+            left++;
+            right--;
+        }       
+
+        name = String.valueOf(str);
+        //for(int i = 0; i < length; i++) {
+            System.out.print(name);
+        //}
+    }
+
+}
+
+
